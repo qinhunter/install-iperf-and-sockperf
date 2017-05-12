3315b99b866538197ded9e21a8981dc83dcafdb2
Hunter <254822498@qq.com>
2017-05-12 09:53:25 +0800

    Create 1.sh
    git clone kernel and checkout v4.6

---
+++ 1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+usernow=`who | cut -d' ' -f1 | sort | tail -n 1`
+
+sudo apt-get update
+sudo apt-get install perl automake autoconf m4 libtool gcc -y
+sudo apt-get install git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache bc vim screen -y
+
+
+#git clone kernel
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+cd linux
+git checkout v4.6
