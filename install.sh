dcfd7772a8122fa8bd09fd8343656a66111d1d9a
Hunter <254822498@qq.com>
2017-05-12 10:02:09 +0800

    Update install.sh
    add variable usernow

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+usernow=`who | cut -d' ' -f1 | sort | tail -n 1`
+
 #install iperf
 
 sudo apt-get update
@@ -9,7 +12,7 @@ sudo apt-get install iperf -y
 
 sudo apt-get install perl automake autoconf m4 libtool gcc -y
 sudo apt-get install git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache bc vim screen -y
-cd ~
+cd /users/$usernow
 git clone https://github.com/Mellanox/sockperf.git
 cd sockperf
 ./autogen.sh 
